1bcae4043a245917127cbfa9b924d2d15b46fccc
PureNukage <roosevelt_stenson@yahoo.com>
2018-12-12 12:00:15 -0600

    visual studio + before massive rewrite

--- app.js
+++ app.js
@@ -1,17 +1,27 @@
 const express = require('express');
 const app = express();
 
-app.set('view engine', 'pug');
+var http = require('http');
+var fs = require('fs');
 
-app.use(express.static(__dirname + '/public'));
-
-app.get('/', (req, res) => {
-	res.render('index', {
-		title: 'Homepage',
-		people: people.profiles
-	});
+const server = http.createServer(function(req, res){
+	console.log("request was made: " + req.url);
+	if(req.url === '/home' || req.url === '/'){
+		res.writeHead(200, {'Content-Type': 'text/html'});
+		fs.createReadStream(__dirname + '/index.html').pipe(res);
+	} else if(req.url === '/contact'){
+		res.writeHead(200, {'Content-Type': 'text/html'});
+		fs.createReadStream(__dirname + '/contact.html').pipe(res);		
+	} else if(req.url === '/api/ninjas'){
+		var ninjas = [{name: 'ryu', age: 29}, {name: 'yoshi', age: 32}]; 
+		res.writeHead(200, {'Content-Type': 'application/json'});
+		res.end(JSON.stringify(ninjas));
+	} else {
+		res.writeHead(404, {'Content-Type': 'text/html'});
+		fs.createReadStream(__dirname + '/404.html').pipe(res);
+	} 
 });
 
-const server = app.listen(3000, () => {
+server.listen(3000, () => {
 	console.log(`Express running -> PORT ${server.address().port}`);
 });
