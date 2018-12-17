18096ccf063c5cc56614e97f3f915d3267bc4a9f
PureNukage <roosevelt_stenson@yahoo.com>
2018-12-17 11:18:45 -0600

    routes/wiki + mongoDB
    + requires mongoDB, hooked up to a database titled "my_database". 'populatedb.js' will load a fresh database with objects.
    
    + added the route to wiki.js

--- app.js
+++ app.js
@@ -3,10 +3,22 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var mongoose = require('mongoose');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var coolRouter = require('./routes/cool');
+var wikiRouter = require('./routes/wiki.js');
+
+//Set up default mongoose connections
+var mongoDB = 'mongodb://127.0.0.1/my_database';
+mongoose.connect(mongoDB);
+//Get mongoose to use the global promise library
+mongoose.Promise = global.Promise;
+//Get the default connection
+var db = mongoose.connection;
+//Bind connection to error event (to get notification of connection errors)
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 var app = express();
 
@@ -23,6 +35,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/users/cool', coolRouter);
+app.use('/wiki', wikiRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
